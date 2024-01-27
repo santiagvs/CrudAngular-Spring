bde0e6366190f702a3f868e1751c7e556fc34163
Santiago <victor.lacsilva0102@gmail.com>
2024-01-26 23:13:07 -0300

    feat: adicionando um JSON de dados para que o service simule um consumo de dados verdadeiro, usando também o RXJS

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent {
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns: string[] = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
+  private readonly API = '/assets/courses.json';
 
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'Frontend' },
-      { _id: '2', name: 'React', category: 'Frontend' },
-      { _id: '3', name: 'Vue', category: 'Frontend' },
-      { _id: '4', name: 'Node', category: 'Backend' },
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap((courses) => console.log(courses))
+    );
   }
 }
